e6df508ab166d12fbc4d26b129aa77355492c764
Samiddin <samiddinravshanov@gmail.com>
2022-12-17 16:39:31 +0500

    Birinchi yuklangan fayllar

---
+++ Controllers/SpeakersController.cs
@@ -0,0 +1,29 @@
+﻿using edumedia.uz.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace edumedia.uz.Controllers
+{
+    public class SpeakersController : Controller
+    { 
+        private readonly ILogger<SpeakersController> _logger;
+        public SpeakersController(ILogger<SpeakersController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Speakers()
+        {
+            return View();
+        }
+        public IActionResult SpeakersDetail() 
+        { 
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
